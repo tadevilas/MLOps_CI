fda711dcb67b2c9d8873b235ad88210a4d44ebd3
VILAS TADE [ TTL <vt929564.ttl@pv.tatamotors>
2025-05-30 14:35:16 +0530

    Corrected test file

--- _test.py
+++ _test.py
@@ -2,33 +2,32 @@ import pytest
 
 # function to check sq
 def square(n):
-    return n**2
+    return n ** 2
 
 # function to check cube
 def cube(n):
-    return n**3
+    return n ** 3
 
-# function to check firth power
+# function to check fifth power
 def fifth_power(n):
-    return n**5
+    return n ** 5
 
-
-# testing the sq fun
+# testing the square function
 def test_square():
-    assert square(2) == 4, "Test failed: Sq of the 2 should be 4"
-    assert square(3) == 9, "Test failed: Sq of the 3 should be 9"
+    assert square(2) == 4, "Test failed: Square of 2 should be 4"
+    assert square(3) == 9, "Test failed: Square of 3 should be 9"
 
-# testing the cube fun
+# testing the cube function
 def test_cube():
-    assert cube(2) == 8, "Test failed: Cube of the 2 should be 8"
-    assert cube(3) == 27, "Test failed: Cube of the 3 should be 27"
+    assert cube(2) == 8, "Test failed: Cube of 2 should be 8"
+    assert cube(3) == 27, "Test failed: Cube of 3 should be 27"
 
+# testing the fifth power function
+def test_fifth_power():
+    assert fifth_power(2) == 32, "Test failed: Fifth Power of 2 should be 32"
+    assert fifth_power(3) == 243, "Test failed: Fifth Power of 3 should be 243"
 
-# testing the Fifth Power fun
-def test_square():
-    assert square(2) == 32, "Test failed: Fifth Power of the 2 should be 32"
-    assert square(3) == 243, "Test failed: Fifth Power of the 3 should be 243"
-
+# test invalid input for square
 def test_invalid_input():
     with pytest.raises(TypeError):
         square('string')
